4c631bf1de3408624b5fbc9f60213fd277064095
Lakshman Chaluvaraju <lchalu@codeaurora.org>
2021-03-10 15:22:00 +0530

    asoc: Use params pointer after NULL check.
    Assign dir and be_id from params pointer after NULL
    check to avoid memory corruption errors.
    
    Change-Id: I9e067e6b3423d6c1f5661e577715bf244c81dc20
    Signed-off-by: Kisan Yadav <quic_kisayada@quicinc.com>

--- asoc/msm-pcm-routing-v2.c
+++ asoc/msm-pcm-routing-v2.c
@@ -31663,9 +31663,9 @@ static int asrc_get_module_location(struct asrc_module_config_params *params,
 					int *copp_index, int *port_id)
 {
 	int ret = 0;
-	int fe_id;
-	int dir = params->dir;
-	int be_id = params->be_id;
+	int fe_id = 0;
+	int dir = 0;
+	int be_id = 0;
 	int copp_idx = 0;
 	unsigned long copp = -1;
 	bool copp_is_found = false;
@@ -31682,6 +31682,8 @@ static int asrc_get_module_location(struct asrc_module_config_params *params,
 	mutex_lock(&routing_lock);
 
 	fe_id = params->fe_id;
+	dir = params->dir;
+	be_id = params->be_id;
 	bedai = &msm_bedais[be_id];
 	if (afe_get_port_type(bedai->port_id) != port_type) {
 		pr_err("%s: port_type not match: be_dai %d type %d\n",
@@ -31847,7 +31849,7 @@ static void get_drift_and_put_asrc(struct work_struct *work)
 	struct asrc_config *p_asrc_cfg = NULL;
 	struct afe_param_id_dev_timing_stats timing_stats = {0};
 	struct asrc_module_config_node *config_node = NULL;
-	struct list_head *ptr, *next;
+	struct list_head *ptr = NULL, *next = NULL;
 
 	delayed_drift_work = to_delayed_work(work);
 	if (NULL == delayed_drift_work) {
@@ -32026,9 +32028,10 @@ static int msm_dai_q6_asrc_config_put(
 		break;
 	case ENABLE_ASRC_DRIFT_HW:
 		idx = get_drift_src_idx(param & ~0x0100); /* group device */
-		if (idx < 0) {
-			pr_err("%s: Trying to access invalid index\n", __func__);
-			return -EINVAL;
+		if (idx < 0 || idx >= DRIFT_SRC_MAX) {
+			pr_err("%s Invalid index: %d\n", __func__, idx);
+			ret = -EINVAL;
+			goto done;
 		}
 		mutex_lock(&asrc_cfg[idx].lock);
 		asrc_cfg[idx].drift_src = param & ~0x0100;
