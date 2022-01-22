9c83a58c689b4b6fade0e33ad6f5430aafe27ba1
Gangadhar S <quic_gangadha@quicinc.com>
2022-01-22 12:35:49 +0530

    ASoC: TDM fractional sample rate support
    Fractional sample rate doesnt work due to GSDC
    not enabled.
    Add vote for audio hw clk for fractional sample
    rate.
    
    Change-Id: I96cbaff02334b5969ab43d1c14c6596dc4036323
    Signed-off-by: Ramu Gottipati <quic_ramug@quicinc.com>

--- asoc/kona.c
+++ asoc/kona.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  * Copyright (c) 2016-2021, The Linux Foundation. All rights reserved.
  */
 
@@ -39,6 +40,7 @@
 #include <dt-bindings/sound/audio-codec-port-types.h>
 #include "codecs/bolero/wsa-macro.h"
 #include "kona-port-config.h"
+#include "msm-common.h"
 
 #define DRV_NAME "kona-asoc-snd"
 #define __CHIPSET__ "KONA "
@@ -132,16 +134,6 @@ enum {
 };
 
 enum {
-	PRIM_MI2S = 0,
-	SEC_MI2S,
-	TERT_MI2S,
-	QUAT_MI2S,
-	QUIN_MI2S,
-	SEN_MI2S,
-	MI2S_MAX,
-};
-
-enum {
 	WSA_CDC_DMA_RX_0 = 0,
 	WSA_CDC_DMA_RX_1,
 	RX_CDC_DMA_RX_0,
@@ -179,26 +171,6 @@ enum {
 	AFE_LOOPBACK_TX_IDX = 0,
 	AFE_LOOPBACK_TX_IDX_MAX,
 };
-struct msm_asoc_mach_data {
-	struct snd_info_entry *codec_root;
-	int usbc_en2_gpio; /* used by gpio driver API */
-	int lito_v2_enabled;
-	struct device_node *dmic01_gpio_p; /* used by pinctrl API */
-	struct device_node *dmic23_gpio_p; /* used by pinctrl API */
-	struct device_node *dmic45_gpio_p; /* used by pinctrl API */
-	struct device_node *mi2s_gpio_p[MI2S_MAX]; /* used by pinctrl API */
-	atomic_t mi2s_gpio_ref_count[MI2S_MAX]; /* used by pinctrl API */
-	struct device_node *us_euro_gpio_p; /* used by pinctrl API */
-	struct pinctrl *usbc_en2_gpio_p; /* used by pinctrl API */
-	struct device_node *hph_en1_gpio_p; /* used by pinctrl API */
-	struct device_node *hph_en0_gpio_p; /* used by pinctrl API */
-	bool is_afe_config_done;
-	struct device_node *fsa_handle;
-	struct clk *lpass_audio_hw_vote;
-	int core_audio_vote_count;
-	u32 tdm_max_slots; /* Max TDM slots used */
-};
-
 struct tdm_port {
 	u32 mode;
 	u32 channel;
@@ -5610,6 +5582,49 @@ static int msm_fe_qos_prepare(struct snd_pcm_substream *substream)
 			  MSM_LL_QOS_VALUE);
 	return 0;
 }
+
+int msm_lpass_audio_hw_vote_req(struct snd_pcm_substream *substream, bool enable)
+{
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_card *card = rtd->card;
+	struct msm_asoc_mach_data *pdata = snd_soc_card_get_drvdata(card);
+	int rc = 0;
+
+	if (enable) {
+		if (pdata->lpass_audio_hw_vote == NULL) {
+			dev_err(rtd->card->dev, "%s: Invalid lpass audio hw node\n",
+				__func__);
+			rc = -EINVAL;
+			goto rtn;
+		}
+		if (pdata->core_audio_vote_count == 0) {
+			rc = clk_prepare_enable(pdata->lpass_audio_hw_vote);
+			if (rc < 0) {
+				dev_err(rtd->card->dev, "%s: audio vote error\n",
+						__func__);
+				goto rtn;
+			}
+		}
+
+		pdata->core_audio_vote_count++;
+	} else {
+		if (pdata->lpass_audio_hw_vote != NULL) {
+			if (--pdata->core_audio_vote_count == 0) {
+				clk_disable_unprepare(
+					pdata->lpass_audio_hw_vote);
+			} else if (pdata->core_audio_vote_count < 0) {
+				pr_err("%s: audio vote mismatch\n", __func__);
+				pdata->core_audio_vote_count = 0;
+			}
+		} else {
+			pr_err("%s: Invalid lpass audio hw node\n", __func__);
+		}
+	}
+
+rtn:
+  return rc;
+}
+EXPORT_SYMBOL(msm_lpass_audio_hw_vote_req);
 
 void mi2s_disable_audio_vote(struct snd_pcm_substream *substream)
 {
