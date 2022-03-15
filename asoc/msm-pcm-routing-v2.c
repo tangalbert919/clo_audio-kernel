417f8f091104d0a60a1085615ea215580eec11e7
Kisan YADAV(Temp) <quic_kisayada@quicinc.com>
2022-03-15 16:32:13 +0530

    soc: Update port_type in asrc_get_module_location
    Initialize port_type to 0 and overwrite it after dir is assigned
    from params to avoid invalid port_type.
    
    Change-Id: I0d0fd8f775a2656c7dc2acdba6e835e06cc67894
    Signed-off-by: Kisan Yadav <quic_kisayada@quicinc.com>

--- asoc/msm-pcm-routing-v2.c
+++ asoc/msm-pcm-routing-v2.c
@@ -31670,8 +31670,7 @@ static int asrc_get_module_location(struct asrc_module_config_params *params,
 	unsigned long copp = -1;
 	bool copp_is_found = false;
 	struct msm_pcm_routing_bdai_data *bedai = NULL;
-	int port_type = (dir == SESSION_TYPE_RX) ? MSM_AFE_PORT_TYPE_RX :
-			       MSM_AFE_PORT_TYPE_TX;
+	int port_type = 0;
 
 	if (NULL == params || NULL == copp_index || NULL == port_id) {
 		pr_err("%s: Invalid params\n", __func__);
@@ -31685,6 +31684,8 @@ static int asrc_get_module_location(struct asrc_module_config_params *params,
 	dir = params->dir;
 	be_id = params->be_id;
 	bedai = &msm_bedais[be_id];
+	port_type = (dir == SESSION_TYPE_RX) ? MSM_AFE_PORT_TYPE_RX :
+			       MSM_AFE_PORT_TYPE_TX;
 	if (afe_get_port_type(bedai->port_id) != port_type) {
 		pr_err("%s: port_type not match: be_dai %d type %d\n",
 			__func__, be_id, port_type);
