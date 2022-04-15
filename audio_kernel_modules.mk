06454514b5678a922b10771fa643ecca4fff93df
sasevs <quic_sasevs@quicinc.com>
2022-04-15 19:30:45 +0530

    Upgrade ELITE audio driver for k5.15
    Add remote proc and dma changes.
    
    Change-Id: I990c669855363ee76d21bbaa0ef5e9ee93da508b

---
+++ audio_kernel_modules.mk
@@ -0,0 +1,14 @@
+# Build audio kernel driver
+AUDIO_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/q6_notifier_dlkm.ko\
+	$(KERNEL_MODULES_OUT)/q6_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/adsp_loader_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/native_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/apr_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/snd_event_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/stub_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/machine_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/platform_dlkm.ko \
+	$(KERNEL_MODULES_OUT)/hdmi_dlkm.ko
+
+
+
